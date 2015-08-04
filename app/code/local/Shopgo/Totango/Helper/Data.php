c1b6a8ee1e1111ab072c7b9c2a592024d3cdf23e
Ammar <ammar@shopgo.me>
2015-08-05 00:15:07 +0300

    add log calls, add phpdoc and improve some functions

--- app/code/local/Shopgo/Totango/Helper/Data.php
+++ app/code/local/Shopgo/Totango/Helper/Data.php
@@ -1,45 +1,146 @@
 <?php
+/**
+ * ShopGo
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    Shopgo
+ * @package     Shopgo_Core
+ * @copyright   Copyright (c) 2015 Shopgo. (http://www.shopgo.me)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
+
+/**
+ * Data helper
+ *
+ * @category    Shopgo
+ * @package     Shopgo_Core
+ * @authors     Ammar <ammar@shopgo.me>
+ *              Emad  <emad@shopgo.me>
+ *              Ahmad <ahmadalkaid@shopgo.me>
+ *              Aya   <aya@shopgo.me>
+ */
 class Shopgo_Totango_Helper_Data extends Shopgo_Core_Helper_Abstract
 {
+    /**
+     * Totango Service URL
+     */
     const SERVICE_URL = 'https://sdr.totango.com/pixel.gif/';
 
+    /**
+     * CONFIG path constant: ENABLED
+     */
     const XML_PATH_TOTANGO_GENERAL_ENABLED = 'shopgo_totango/general/enabled';
 
+
+    /**
+     * CONFIG path constant: SERVICE_ID
+     */
     const XML_PATH_TOTANGO_GENERAL_SERVICE_ID   = 'shopgo_totango/general/service_id';
+
+    /**
+     * CONFIG path constant: ACCOUNT_ID
+     */
     const XML_PATH_TOTANGO_GENERAL_ACCOUNT_ID   = 'shopgo_totango/general/account_id';
+
+    /**
+     * CONFIG path constant: ACCOUNT_NAME
+     */
     const XML_PATH_TOTANGO_GENERAL_ACCOUNT_NAME = 'shopgo_totango/general/account_name';
+
+    /**
+     * CONFIG path constant: USER_ID
+     */
     const XML_PATH_TOTANGO_GENERAL_USER_ID      = 'shopgo_totango/general/user_id';
 
+
+    /**
+     * CONFIG path constant: TRACKERS
+     */
     const XML_PATH_TOTANGO_TRACKERS = 'shopgo_totango/trackers/';
 
 
+    /**
+     * Log file name
+     *
+     * @var string
+     */
+    protected $_logFile = 'shopgo_totango.log';
+
+    /**
+     * Trackers names
+     *
+     * @var array
+     */
     private static $_trackers = array(
         'product', 'category', 'attribute',
         'shipping_payment', 'order', 'admin_login'
     );
 
 
+    /**
+     * Check whether the extension is enabled or not
+     *
+     * @return bool
+     */
     public function isEnabled()
     {
         return Mage::getStoreConfig(self::XML_PATH_TOTANGO_GENERAL_ENABLED);
     }
 
+    /**
+     * Check whether a specific tracker is enabled or not
+     *
+     * @param string $tracker
+     * @return bool
+     */
     public function isTrackerEnabled($tracker)
     {
-        return in_array($tracker, self::getTrackers())
-            && Mage::getStoreConfig(self::XML_PATH_TOTANGO_TRACKERS . $tracker);
+        $result = false;
+
+        if (in_array($tracker, self::getTrackers())) {
+            $result = Mage::getStoreConfig(self::XML_PATH_TOTANGO_TRACKERS . $tracker);
+
+            if (!$result) {
+                $this->log(sprintf('Tracker "%s" is disabled', $tracker));
+            }
+        } else {
+            $this->log(sprintf('Tracker "%s" is invalid', $tracker));
+        }
+
+        return $result;
     }
 
+    /**
+     * Get a list of predefined trackers
+     *
+     * @return array
+     */
     public static function getTrackers()
     {
         return self::$_trackers;
     }
 
+    /**
+     * Log data user-activity event or attribute-updates
+     *
+     * @param string $event
+     * @param array $data
+     * @return string|bool
+     */
     public function track($event, $data)
     {
+        $result = false;
+
         if (empty($data)) {
-            return false;
+            $this->log('There is no tracking data provided');
+            return $result;
         }
 
         $params = array(
@@ -55,40 +156,71 @@ class Shopgo_Totango_Helper_Data extends Shopgo_Core_Helper_Abstract
 
         switch ($event) {
             case 'user-activity':
-                $params['sdr_u'] = Mage::getStoreConfig(self::XML_PATH_TOTANGO_GENERAL_USER_ID);
-                $params['sdr_a'] = $data['action'];
-                $params['sdr_m'] = $data['module'];
+                if (isset($data['action']) && isset($data['module'])) {
+                    $params['sdr_u'] = Mage::getStoreConfig(self::XML_PATH_TOTANGO_GENERAL_USER_ID);
+                    $params['sdr_a'] = $data['action'];
+                    $params['sdr_m'] = $data['module'];
+                    $result = true;
+                } else {
+                    $this->log('Insufficient tracking data');
+                }
                 break;
             case 'account-attribute':
-                $params['sdr_o.' . $data['attribute']['name']] = $data['attribute']['value'];
+                if (isset($data['attribute']['name']) && isset($data['attribute']['value'])) {
+                    $params['sdr_o.' . $data['attribute']['name']] = $data['attribute']['value'];
+                    $result = true;
+                } else {
+                    $this->log('Insufficient tracking data');
+                }
                 break;
             case 'user-attribute':
-                $params['sdr_u'] = Mage::getStoreConfig(self::XML_PATH_TOTANGO_GENERAL_USER_ID);
-                $params['sdr_u.' . $data['attribute']['name']] = $data['attribute']['value'];
+                if (isset($data['attribute']['name']) && isset($data['attribute']['value'])) {
+                    $params['sdr_u'] = Mage::getStoreConfig(self::XML_PATH_TOTANGO_GENERAL_USER_ID);
+                    $params['sdr_u.' . $data['attribute']['name']] = $data['attribute']['value'];
+                    $result = true;
+                } else {
+                    $this->log('Insufficient tracking data');
+                }
                 break;
-            default:
-                return false;
         }
 
-        return $this->sendRequest($params);
+        return $result ? $this->sendRequest($params) : $result;
     }
 
+    /**
+     * Send request to Totango
+     *
+     * @param array $params
+     * @return string|bool
+     */
     public function sendRequest($params)
     {
+        $result = false;
+
         if (empty($params)) {
-            return false;
+            $this->log('Could not send a request with empty parameters');
+            return $result;
         }
 
-        $result     = '';
         $url        = self::SERVICE_URL;
         $httpClient = new Varien_Http_Client();
 
-        $result = $httpClient
-            ->setUri($url)
-            ->setHeaders('Content-Type: image/gif')
-            ->setParameterPost($params)
-            ->request(Varien_Http_Client::POST)
-            ->getBody();
+        try {
+            $response = $httpClient
+                ->setUri($url)
+                ->setHeaders('Content-Type: image/gif')
+                ->setParameterPost($params)
+                ->request(Varien_Http_Client::POST);
+
+            if ($response->isSuccessful()) {
+                $result = $response->getBody();
+            } else {
+                $this->log('"Send Request" response was unsuccessful');
+            }
+        } catch (Exception $e) {
+            $this->log(sprintf('[Send Request Exception]: %s', $e->getMessage()));
+            $this->log($e, 'exception');
+        }
 
         return $result;
     }
