b393ef257bd9490b1dbe27aabe23ed888a7dce90
Aya_Mousa <aya@shopgo.me>
2015-08-10 20:11:04 +0300

    add enable/disable each order type

--- app/code/local/Shopgo/Totango/Model/Observer.php
+++ app/code/local/Shopgo/Totango/Model/Observer.php
@@ -1,37 +1,41 @@
 <?php
 class Shopgo_Totango_Model_Observer
 {
+    public function getConfigData($key)
+    {
+        return Mage::getStoreConfig('shopgo_totango/trackers/' . $key);
+    }
+    
     public function PostOrderData(Varien_Event_Observer $observer)
     {
-        $CompletedOrders = Mage::getModel('sales/order')->getCollection()->addAttributeToFilter('status', array(
-            'eq' => Mage_Sales_Model_Order::STATE_COMPLETE
-        ));
-        
-        $CanceledOrders = Mage::getModel('sales/order')->getCollection()->addAttributeToFilter('status', array(
-            'eq' => Mage_Sales_Model_Order::STATE_CANCELED
-        ));
-        
-        $CompletedOrdersSize = $CompletedOrders->getSize();
-        
-        $CanceledOrdersSize = $CanceledOrders->getSize();
-        
-        $order = $observer->getOrder();
-        if ($order->getState() == Mage_Sales_Model_Order::STATE_COMPLETE)
+        if ($this->getConfigData('complete_order'))
         {
-            Mage::log('Completed Orders: ' . $CompletedOrdersSize);
-            
-            Mage::helper('totango')->track('account-attribute', array(
-                'CompletedOrders' => $CompletedOrders
+            $CompletedOrders     = Mage::getModel('sales/order')->getCollection()->addAttributeToFilter('status', array(
+                'eq' => Mage_Sales_Model_Order::STATE_COMPLETE
             ));
+            $CompletedOrdersSize = $CompletedOrders->getSize();
+            $order               = $observer->getOrder();
+            if ($order->getState() == Mage_Sales_Model_Order::STATE_COMPLETE)
+            {
+                Mage::helper('totango')->track('account-attribute', array(
+                    'CompletedOrders' => $CompletedOrders
+                ));
+            }
         }
         
-        if ($order->getState() == Mage_Sales_Model_Order::STATE_CANCELED)
+        if ($this->getConfigData('canceled_order'))
         {
-            Mage::log('Canceled Orders: ' . $CanceledOrdersSize);
-            
-            Mage::helper('totango')->track('account-attribute', array(
-                'CanceledOrders' => $CanceledOrders
+            $CanceledOrders     = Mage::getModel('sales/order')->getCollection()->addAttributeToFilter('status', array(
+                'eq' => Mage_Sales_Model_Order::STATE_CANCELED
             ));
+            $CanceledOrdersSize = $CanceledOrders->getSize();
+            $order              = $observer->getOrder();
+            if ($order->getState() == Mage_Sales_Model_Order::STATE_CANCELED)
+            {
+                Mage::helper('totango')->track('account-attribute', array(
+                    'CanceledOrders' => $CanceledOrders
+                ));
+            }
         }
         
     }
