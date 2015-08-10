f5096ce605cdb7cfc0f77d371a22c8e41b3faa98
Aya_Mousa <aya@shopgo.me>
2015-08-10 20:25:30 +0300

    Edit indentation style, so Ammar doesn't go crazy!

--- app/code/local/Shopgo/Totango/Model/Observer.php
+++ app/code/local/Shopgo/Totango/Model/Observer.php
@@ -8,30 +8,26 @@ class Shopgo_Totango_Model_Observer
     
     public function PostOrderData(Varien_Event_Observer $observer)
     {
-        if ($this->getConfigData('complete_order'))
-        {
+        if ($this->getConfigData('complete_order')) {
             $CompletedOrders     = Mage::getModel('sales/order')->getCollection()->addAttributeToFilter('status', array(
                 'eq' => Mage_Sales_Model_Order::STATE_COMPLETE
             ));
             $CompletedOrdersSize = $CompletedOrders->getSize();
             $order               = $observer->getOrder();
-            if ($order->getState() == Mage_Sales_Model_Order::STATE_COMPLETE)
-            {
+            if ($order->getState() == Mage_Sales_Model_Order::STATE_COMPLETE) {
                 Mage::helper('totango')->track('account-attribute', array(
                     'CompletedOrders' => $CompletedOrders
                 ));
             }
         }
         
-        if ($this->getConfigData('canceled_order'))
-        {
+        if ($this->getConfigData('canceled_order')) {
             $CanceledOrders     = Mage::getModel('sales/order')->getCollection()->addAttributeToFilter('status', array(
                 'eq' => Mage_Sales_Model_Order::STATE_CANCELED
             ));
             $CanceledOrdersSize = $CanceledOrders->getSize();
             $order              = $observer->getOrder();
-            if ($order->getState() == Mage_Sales_Model_Order::STATE_CANCELED)
-            {
+            if ($order->getState() == Mage_Sales_Model_Order::STATE_CANCELED) {
                 Mage::helper('totango')->track('account-attribute', array(
                     'CanceledOrders' => $CanceledOrders
                 ));
